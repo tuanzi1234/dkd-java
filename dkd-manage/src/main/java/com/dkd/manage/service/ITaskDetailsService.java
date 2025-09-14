2120435610e57fb4d89cec32e387011748dbcfe6
tuanzi <lvbohan2003@163.com>
2025-09-15 03:21:28 +0800

    工单管理初始化

---
+++ dkd-manage/src/main/java/com/dkd/manage/service/ITaskDetailsService.java
@@ -0,0 +1,61 @@
+package com.dkd.manage.service;
+
+import java.util.List;
+import com.dkd.manage.domain.TaskDetails;
+
+/**
+ * 工单详情，只有补货工单才有Service接口
+ * 
+ * @author ruoyi
+ * @date 2025-09-15
+ */
+public interface ITaskDetailsService 
+{
+    /**
+     * 查询工单详情，只有补货工单才有
+     * 
+     * @param detailsId 工单详情，只有补货工单才有主键
+     * @return 工单详情，只有补货工单才有
+     */
+    public TaskDetails selectTaskDetailsByDetailsId(Long detailsId);
+
+    /**
+     * 查询工单详情，只有补货工单才有列表
+     * 
+     * @param taskDetails 工单详情，只有补货工单才有
+     * @return 工单详情，只有补货工单才有集合
+     */
+    public List<TaskDetails> selectTaskDetailsList(TaskDetails taskDetails);
+
+    /**
+     * 新增工单详情，只有补货工单才有
+     * 
+     * @param taskDetails 工单详情，只有补货工单才有
+     * @return 结果
+     */
+    public int insertTaskDetails(TaskDetails taskDetails);
+
+    /**
+     * 修改工单详情，只有补货工单才有
+     * 
+     * @param taskDetails 工单详情，只有补货工单才有
+     * @return 结果
+     */
+    public int updateTaskDetails(TaskDetails taskDetails);
+
+    /**
+     * 批量删除工单详情，只有补货工单才有
+     * 
+     * @param detailsIds 需要删除的工单详情，只有补货工单才有主键集合
+     * @return 结果
+     */
+    public int deleteTaskDetailsByDetailsIds(Long[] detailsIds);
+
+    /**
+     * 删除工单详情，只有补货工单才有信息
+     * 
+     * @param detailsId 工单详情，只有补货工单才有主键
+     * @return 结果
+     */
+    public int deleteTaskDetailsByDetailsId(Long detailsId);
+}
