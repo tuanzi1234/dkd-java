34ced951519a8200ef200b427981f0507b46acec
tuanzi <lvbohan2003@163.com>
2025-09-15 19:38:21 +0800

    查询工单列表

--- dkd-manage/src/main/java/com/dkd/manage/service/impl/TaskServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/TaskServiceImpl.java
@@ -1,7 +1,9 @@
 package com.dkd.manage.service.impl;
 
 import java.util.List;
+
 import com.dkd.common.utils.DateUtils;
+import com.dkd.manage.domain.vo.TaskVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.TaskMapper;
@@ -10,87 +12,91 @@ import com.dkd.manage.service.ITaskService;
 
 /**
  * 工单Service业务层处理
- * 
+ *
  * @author ruoyi
  * @date 2025-09-15
  */
 @Service
-public class TaskServiceImpl implements ITaskService 
-{
+public class TaskServiceImpl implements ITaskService {
     @Autowired
     private TaskMapper taskMapper;
 
     /**
      * 查询工单
-     * 
+     *
      * @param taskId 工单主键
      * @return 工单
      */
     @Override
-    public Task selectTaskByTaskId(Long taskId)
-    {
+    public Task selectTaskByTaskId(Long taskId) {
         return taskMapper.selectTaskByTaskId(taskId);
     }
 
     /**
      * 查询工单列表
-     * 
+     *
      * @param task 工单
      * @return 工单
      */
     @Override
-    public List<Task> selectTaskList(Task task)
-    {
+    public List<Task> selectTaskList(Task task) {
         return taskMapper.selectTaskList(task);
     }
 
     /**
      * 新增工单
-     * 
+     *
      * @param task 工单
      * @return 结果
      */
     @Override
-    public int insertTask(Task task)
-    {
+    public int insertTask(Task task) {
         task.setCreateTime(DateUtils.getNowDate());
         return taskMapper.insertTask(task);
     }
 
     /**
      * 修改工单
-     * 
+     *
      * @param task 工单
      * @return 结果
      */
     @Override
-    public int updateTask(Task task)
-    {
+    public int updateTask(Task task) {
         task.setUpdateTime(DateUtils.getNowDate());
         return taskMapper.updateTask(task);
     }
 
     /**
      * 批量删除工单
-     * 
+     *
      * @param taskIds 需要删除的工单主键
      * @return 结果
      */
     @Override
-    public int deleteTaskByTaskIds(Long[] taskIds)
-    {
+    public int deleteTaskByTaskIds(Long[] taskIds) {
         return taskMapper.deleteTaskByTaskIds(taskIds);
     }
 
     /**
      * 删除工单信息
-     * 
+     *
      * @param taskId 工单主键
      * @return 结果
      */
     @Override
-    public int deleteTaskByTaskId(Long taskId)
-    {
+    public int deleteTaskByTaskId(Long taskId) {
         return taskMapper.deleteTaskByTaskId(taskId);
+    }
+
+    /**
+     * 查询工单列表
+     *
+     * @param task 工单
+     * @return 工单
+     */
+    @Override
+    public List<TaskVo> selectTaskVoList(Task task) {
+        return taskMapper.selectTaskVoList(task);
     }
 }
